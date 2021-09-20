a1a98740c57b2056e70d7f8b406e3f41031c8dd3
Khang <khang.job@gmail.com>
2021-09-20 21:03:51 +0700

    add hotwire

--- app/controllers/chat_rooms_controller.rb
+++ app/controllers/chat_rooms_controller.rb
@@ -3,6 +3,6 @@ class ChatRoomsController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    @chat_rooms = current_user.chat_rooms
+    @chat_rooms = current_user.chat_room_users.includes(:chat_room)
   end
 end
--- app/models/chat_room_user.rb
+++ app/models/chat_room_user.rb
@@ -11,4 +11,7 @@
 class ChatRoomUser < ApplicationRecord
   belongs_to :chat_room
   belongs_to :user
+
+  after_create_commit -> { broadcast_action_later_to [:chat_rooms, self.user_id], action: :prepend, target: :room_list }
+  after_destroy_commit -> { broadcast_remove_to [:chat_rooms, self.user_id] }
 end
